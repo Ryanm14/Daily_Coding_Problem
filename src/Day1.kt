39f1f2611037380d00c9b7bbe638fa40056b0b5b
Ryan Miles <ryanm1114@gmail.com>
2019-04-09 12:51:45 -0400

    Day 1

--- src/Day1.kt
+++ src/Day1.kt
@@ -1,3 +1,22 @@
-class Day1 {
+/**
+ * Given a list of numbers and a number k, return whether any two numbers from the list add up to k.
 
+For example, given [10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17.
+
+Bonus: Can you do this in one pass?
+ */
+fun main() {
+    print(twoAdd(intArrayOf(10, 15, 3, 7), 13))
+}
+
+private fun twoAdd(list: IntArray, k: Int): Boolean {
+    val hashmap = HashMap<Int, Int>()
+
+    list.forEachIndexed{ index, int ->
+        if (hashmap.containsKey(k - int)){
+            return true
+        }
+        hashmap[int] = int
+    }
+    return false
 }
